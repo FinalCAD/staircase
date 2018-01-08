6c22620126e6ba6af912f4a4645d144a6fa759cf
Joel AZEMAR <joel.azemar@gmail.com>
2018-01-08 14:53:29 +0100

    Add move method to Grid

--- lib/composer/lib/grid.rb
+++ lib/composer/lib/grid.rb
@@ -8,6 +8,73 @@ module Composer
         @number_of_images = number_of_images
         @columns = Math.sqrt(number_of_images).ceil
         @rows    = Math.sqrt(number_of_images).round
+
+        @column_cursor = Cursor.new(columns)
+        @row_cursor    = Cursor.new(rows)
+      end
+
+      def move
+        Point.new(y: next_column, x: next_row)
+      end
+
+      private
+
+      attr_reader :column_cursor, :row_cursor
+
+      def next_column
+        column_cursor.next_position { change_row! }
+      end
+
+      def next_row
+        reset_row! if column_changed?
+        @current_row ||= row_cursor.next_position
+      end
+
+      def column_changed?
+        column_cursor.current_position == columns
+      end
+
+      def reset_row!
+        @row_cursor = Cursor.new(rows)
+      end
+
+      def change_row!
+        @current_row = nil
+      end
+
+      class Cursor
+        def initialize(size)
+          @size = size
+          @position = -1
+        end
+
+        def next_position
+          reset if reached_bound?
+          if block_given?
+            yield if reinitiated?
+          end
+          @position += 1
+        end
+
+        def current_position
+          @position
+        end
+
+        private
+
+        def reset
+          @position = -1
+        end
+
+        def reinitiated?
+          @position == -1
+        end
+
+        attr_reader :size
+
+        def reached_bound?
+          (@position + 1) >= size
+        end
       end
 
     end
--- spec/composer/lib/grid_spec.rb
+++ spec/composer/lib/grid_spec.rb
@@ -14,4 +14,33 @@ describe Composer::Lib::Grid do
       expect(g.rows).to eql(grid[:rows])
     end
   end
+
+  describe '#move' do
+
+    subject { described_class.new(6) }
+
+    it do
+      expect(subject.columns).to eql(3)
+      expect(subject.rows).to eql(2)
+    end
+
+    it do
+      {
+        1 => { column: 0, row: 0 },
+        2 => { column: 1, row: 0 },
+        3 => { column: 2, row: 0 },
+        4 => { column: 0, row: 1 },
+        5 => { column: 1, row: 1 },
+        6 => { column: 2, row: 1 },
+        7 => { column: 0, row: 0 },
+        8 => { column: 1, row: 0 },
+        9 => { column: 2, row: 0 }
+      }.each do |iteration, coordinate|
+        position = subject.move
+
+        expect(position.column.to_i).to eql(coordinate[:column])
+        expect(position.row.to_i).to    eql(coordinate[:row])
+      end
+    end
+  end
 end
